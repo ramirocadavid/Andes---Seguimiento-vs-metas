db7eddcaa9939a32c30e94e0c9af8b393c4803d3
ramirocadavid <ramirocadavid@gmail.com>
2017-12-27 17:19:37 -0500

    Agregué drivers de cumplimiento de prácticas

--- cump_metas.R
+++ cump_metas.R
@@ -1,7 +1,7 @@
-# Importar datos
+# Importar datos --------------------------------------------------------------------
+
 datos <- read.csv("../Cumplimiento vs metas.csv", na.strings = "#N/A")
 str(datos)
-
 
 
 # Prácticas que NO se cumplían en LB ------------------------------------------------
@@ -34,6 +34,14 @@ pract.cump.lb.prod <- pract.cump.lb / n.productores
 datos.c <- datos[datos$cumple_lb == 1, ]
 pract.cump.lb.seg <- sum(datos.c$cumple_seg, na.rm = TRUE) / n.productores
 
+# Por práctica
+library(dplyr)
+cump.lb.seg.practica <- aggregate(select(datos.c, cumple_lb, cumple_seg), 
+                                  by = list(datos.c$practica), FUN = sum, na.rm = TRUE)
+cump.lb.seg.practica <- data.frame(cump.lb.seg.practica,
+                                   diferencia = cump.lb.seg.practica$cumple_seg - 
+                                         cump.lb.seg.practica$cumple_lb)
+
 
 # Prácticas que no cumplían y no meta -----------------------------------------------
 
@@ -45,6 +53,32 @@ pract.nocump.nometa.prod <- pract.nocump.nometa / n.productores
 datos.nc.nm <- datos[datos$cumple_lb == 0 & datos$meta == 0, ]
 pract.nocump.nometa.seg <- sum(datos.nc.nm$cumple_seg, na.rm = TRUE)
 pract.nocump.nometa.seg.prod <- pract.nocump.nometa.seg / n.productores
+
+# Por práctica
+ncnm.seg.practica <- aggregate(select(datos.nc.nm, cumple_lb, cumple_seg), 
+                                  by = list(datos.nc.nm$practica), FUN = sum, na.rm = TRUE)
+ncnm.seg.practica <- data.frame(ncnm.seg.practica,
+                                   diferencia = ncnm.seg.practica$cumple_seg - 
+                                      ncnm.seg.practica$cumple_lb)
+
+
+# Prácticas que no se cumplían y Sí meta --------------------------------------------
+
+# Número de productores
+pract.ncm <- nrow(datos[datos$cumple_lb == 0 & datos$meta == 1, ])
+pract.ncm.prod <- pract.ncm / n.productores
+
+# Prácticas cumplidas en seguimiento por productor
+datos.ncm <- datos[datos$cumple_lb == 0 & datos$meta == 1, ]
+pract.ncm.seg <- sum(datos.ncm$cumple_seg, na.rm = TRUE)
+pract.ncm.seg.prod <- pract.ncm.seg / n.productores
+
+# Por práctica
+ncm.seg.practica <- aggregate(select(datos.ncm, cumple_lb, cumple_seg), 
+                               by = list(datos.ncm$practica), FUN = sum, na.rm = TRUE)
+ncm.seg.practica <- data.frame(ncm.seg.practica,
+                                diferencia = ncm.seg.practica$cumple_seg - 
+                                      ncm.seg.practica$cumple_lb)
 
 
 # Cumplimiento de prácticas con respecto a metas ------------------------------------
